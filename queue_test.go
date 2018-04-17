8eef527995731e33b09c225dcff4f79bbde88306
JasonQu <qjpcpu@gmail.com>
2018-04-17 14:39:09 +0800

    rename low level API

--- queue_test.go
+++ queue_test.go
@@ -8,13 +8,13 @@ import (
 func TestIO(t *testing.T) {
 	conn, _ := redis.Dial("tcp", ":6379")
 	msg := "first message"
-	Add(conn, "qio", msg, 0)
-	str, err := PopString(conn, "qio", 0)
+	AddDelay(conn, "qio", msg, 0)
+	str, err := PopDelayString(conn, "qio", 0)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(str)
-	t.Log(PopString(conn, "qio", 0))
+	t.Log(PopDelayString(conn, "qio", 0))
 }
 
 func TestJSON(t *testing.T) {
@@ -23,14 +23,14 @@ func TestJSON(t *testing.T) {
 		Name string `json:"name"`
 		Age  int
 	}
-	AddJSON(conn, "qjs", obj{Name: "jack", Age: 11}, 0)
+	AddDelayJSON(conn, "qjs", obj{Name: "jack", Age: 11}, 0)
 	var o obj
-	if err := PopJSON(conn, "qjs", 0, &o); err != nil {
+	if err := PopDelayJSON(conn, "qjs", 0, &o); err != nil {
 		t.Fatal(err)
 	}
 	if o.Name != "jack" || o.Age != 11 {
 		t.Fatal(o)
 	}
-	AddJSON(conn, "qjs", obj{Name: "jack", Age: 119}, 0)
-	t.Log(PopString(conn, "qjs", 0))
+	AddDelayJSON(conn, "qjs", obj{Name: "jack", Age: 119}, 0)
+	t.Log(PopDelayString(conn, "qjs", 0))
 }
